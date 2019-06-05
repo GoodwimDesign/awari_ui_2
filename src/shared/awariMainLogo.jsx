1bbf3f444cc947f66edf63a2f1fc2390d9d7dac0
Ray Sanders <raymond.sanders@gmail.com>
2019-06-04 21:02:43 -0500

    Get Create React App Working

---
+++ src/shared/awariMainLogo.jsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export default awariMainLogo = () => (
+  <div className="awari-logo__login">
+    <img src="../images/logos/AwariMainLogoTypeColor.png" alt="Awari" />
+  </div>
+);
