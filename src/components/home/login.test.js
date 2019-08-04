89ed7eab426280d76ee7b43468ab5cb59c565537
Ray Sanders <raymond.sanders@gmail.com>
2019-08-04 12:43:13 -0500

    Add login component tests

--- src/components/home/login.test.js
+++ src/components/home/login.test.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Login from './login';
 import AwariMainLogo from '../shared/awariMainLogo';
+import LoginForm from './loginForm';
+import AppConfig from '../../appConfig';
 
 describe('login component', () => {
-  it('renders without crashing', () => {
+  it('renders the correct things', () => {
     const testObject = shallow(<Login />);
 
-    expect(testObject.find(AwariMainLogo).props()).toHaveLength(1);
+    expect(testObject.find(AwariMainLogo)).toHaveLength(1);
+    expect(testObject.find(LoginForm)).toHaveLength(1);
+    expect(testObject.find('div.login-tagline').text()).toEqual(AppConfig.labels.login.tagline);
   });
 });
