d38f86eb668a7685546bf2e7b4e2adcd8e920f7a
Ray Sanders <raymond.sanders@gmail.com>
2019-08-04 22:35:51 -0500

    Adding dashboard component

---
+++ src/components/dashboard/dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './dashboard.scss';
+
+const Dashboard = () => (
+  <div className="dashboard-container">
+    <div className="left-side">Sup</div>
+    <div className="middle">Hi</div>
+    <div className="right-side">Chello</div>
+  </div>
+);
+
+export default Dashboard;
