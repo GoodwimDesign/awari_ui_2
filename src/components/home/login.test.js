26e78f33549e3b6d1ea8ab857238625fbed301e0
Ray Sanders <raymond.sanders@gmail.com>
2019-08-10 21:20:32 -0500

    Add .editorconfig; Changes line endings to Unix style and fix them all

--- src/components/home/login.test.js
+++ src/components/home/login.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Login from './login';
 import AwariMainLogo from '../shared/awariMainLogo';
-import LoginForm from './loginForm';
 import AppConfig from '../../appConfig';
 
 describe('login component', () => {
@@ -10,7 +9,6 @@ describe('login component', () => {
     const testObject = shallow(<Login />);
 
     expect(testObject.find(AwariMainLogo)).toHaveLength(1);
-    expect(testObject.find(LoginForm)).toHaveLength(1);
     expect(testObject.find('div.login-tagline').text()).toEqual(AppConfig.labels.login.tagline);
   });
 });
