7ad740a12326759fc1c4e18de4eef26a6953488f
Ray Sanders <raymond.sanders@gmail.com>
2019-10-17 22:03:17 -0500

    Remove uneeded app-content class

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,18 @@ import { withAuth } from '@okta/okta-react';
 import useAuth from './useAuth';
 import Login from './components/home/login';
 
-import './app.scss';
 
 const App = withAuth(({ auth }) => {
   const [authenticated, user] = useAuth(auth);
 
   return (
-    <div className="app-content">
+    <>
       <Login
         auth={auth}
         authenticated={authenticated}
         user={user}
       />
-    </div>
+    </>
   );
 });
 
