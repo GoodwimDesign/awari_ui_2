9ae5111412a3baf5b26c4a56de8f285f0d401dc0
Ray Sanders <raymond.sanders@gmail.com>
2019-10-17 22:59:05 -0500

    Display logo in left pane container

--- src/components/dashboard/leftPane.jsx
+++ src/components/dashboard/leftPane.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import LogoThirtyPercent from '../../images/logos/AwariMainLogoTypeColor_30percent.png';
 
 import './leftPane.scss';
 
 const LeftPane = () => (
-  <>
-    Sup
-  </>
+  <div className="left-pane-container">
+    <img className="logo" src={LogoThirtyPercent} alt="Awari Logo" />
+  </div>
 );
 
 export default LeftPane;
--- src/components/dashboard/leftPane.test.js
+++ src/components/dashboard/leftPane.test.js
@@ -1,5 +1,14 @@
-describe('left pane', () => {
-  it.skip('should do a thing', () => {
+import React from 'react';
+import { mount } from 'enzyme';
+import LeftPane from './leftPane';
+import LogoThirtyPercent from '../../images/logos/AwariMainLogoTypeColor_30percent.png';
 
+describe('left pane', () => {
+  it('contains an awari logo image', () => {
+    const testObject = mount(<LeftPane />);
+
+    expect(testObject.find('img').length).toEqual(1);
+    expect(testObject.find('img').props().src).toEqual(LogoThirtyPercent);
+    expect(testObject.find('img').props().alt).toEqual('Awari Logo');
   });
 });
