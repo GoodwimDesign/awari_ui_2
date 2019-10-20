a558233c2c6e3dc131684a5ff0e9d7988edd66cf
Ray Sanders <raymond.sanders@gmail.com>
2019-10-20 16:08:50 -0500

    Styling middle pane container

--- src/components/dashboard/middlePane.jsx
+++ src/components/dashboard/middlePane.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import './middlePane.scss';
+
 const MiddlePane = () => (
-  <>
+  <div className="middle-pane-container">
     <h1>Dashboard</h1>
-  </>
+  </div>
 );
 
 export default MiddlePane;
