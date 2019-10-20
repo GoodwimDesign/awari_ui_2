5b97f80cc8c726e4d9bb16de405d9da3b3f1deee
Ray Sanders <raymond.sanders@gmail.com>
2019-10-20 17:06:45 -0500

    Add Dashboard component

--- src/components/dashboard/middlePane.jsx
+++ src/components/dashboard/middlePane.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
 import './middlePane.scss';
+import DashboardData from './dashboardData';
 
 const MiddlePane = () => (
   <div className="middle-pane-container">
     <h1>Dashboard</h1>
+    <div>
+      <DashboardData />
+    </div>
   </div>
 );
 
--- src/components/dashboard/middlePane.test.js
+++ src/components/dashboard/middlePane.test.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import MiddlePane from './middlePane';
+import DashboardData from './dashboardData';
 
 describe('middlePane', () => {
-  it('contains a dashboard header', () => {
-    const testObject = mount(<MiddlePane />);
+  let testObject;
 
+  beforeEach(() => {
+    testObject = mount(<MiddlePane />);
+  });
+
+  it('contains a dashboard header', () => {
     expect(testObject.find('h1').length).toEqual(1);
     expect(testObject.find('h1').text()).toEqual('Dashboard');
+  });
+
+  it('contains a dashboard data component', () => {
+    expect(testObject.find(DashboardData).length).toEqual(1);
   });
 });
