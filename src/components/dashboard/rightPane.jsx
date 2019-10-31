a7ef6ef6adf2507dad8b448462ba59e81f37fff9
Ray Sanders <raymond.sanders@gmail.com>
2019-10-30 23:22:12 -0500

    Styling right pane

--- src/components/dashboard/rightPane.jsx
+++ src/components/dashboard/rightPane.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import './rightPane.scss';
+
 const RightPane = () => (
-  <div>foo</div>
+  <div className="right-pane-container">
+    <div className="user-widget">
+      <div className="user-name">
+        Ray Sanders
+      </div>
+      <div className="logout-button">
+        Logout
+      </div>
+    </div>
+  </div>
 );
 
 export default RightPane;
